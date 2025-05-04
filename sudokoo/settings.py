8a48cdbc59abe0128671878b55dcded530efe6e2
Geoff Mackey <geoffmackey2@gmail.com>
2025-05-04 16:23:10 -0400

    debug

--- sudokoo/settings.py
+++ sudokoo/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-97w)y$=pal9yto(r*m**n0*reu*6lusd7payz^h4bo1zb=dm)y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
