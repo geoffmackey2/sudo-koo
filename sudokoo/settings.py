f85d521219aa68b05c37b024d82a86e3bdfab25f
Geoff Mackey <geoffmackey2@gmail.com>
2025-05-04 14:55:08 -0400

    final fix I think

--- sudokoo/settings.py
+++ sudokoo/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET')
+SECRET_KEY = 'django-insecure-97w)y$=pal9yto(r*m**n0*reu*6lusd7payz^h4bo1zb=dm)y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
